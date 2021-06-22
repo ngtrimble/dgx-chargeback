f92ff85ad9e5efb0c02bce49254b68efde0b9c9b
Kalen Peterson <kalen.peterson@siriuscom.com>
2021-06-22 08:10:47 -0700

    Added test template

---
+++ src/tests/test_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+../chargeback.py \
+    --slurm-job-prev-day 3 \
+    --slurm-cluster-name '' \
+    --slurm-db-host '' \
+    --slurm-db-port '' \
+    --slurm-db-username '' \
+    --slurm-db-password '' \
+    --chargeback-db-host '' \
+    --chargeback-db-port '' \
+    --chargeback-db-schema-name '' \
+    --chargeback-db-table-name '' \
+    --chargeback-db-username '' \
+    --chargeback-db-password '' \
+    --email-smtp-host '' \
+    --email-smtp-port '' \
+    --email-to-address '' \
+    --email-from-address '' \
